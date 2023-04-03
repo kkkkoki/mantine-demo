e81455676cffd51b5b520ef07aec3a308ab73ad2
kkkkoki <koki_inoue45@icloud.com>
2023-04-03 20:33:10 +0900

    drawer

---
+++ src/pages/home.tsx
@@ -0,0 +1,34 @@
+import { ReactElement } from 'react';
+import { Drawer } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { NextPageWithLayout } from './_app';
+import DrawerComponent from '@/components/base/Drawer/Drawer';
+import Layout from '@/components/base/Layout/Layout';
+
+const Home: NextPageWithLayout = () => {
+  const [opened, { toggle }] = useDisclosure(false);
+
+  return (
+    <>
+      <div>Home</div>
+      <Drawer
+        opened={opened}
+        onClose={toggle}
+        title="search"
+        transitionProps={{
+          transition: 'skew-down',
+          duration: 150,
+          timingFunction: 'linear',
+        }}
+      >
+        <DrawerComponent />
+      </Drawer>
+    </>
+  );
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default Home;
