562eda4573c8fc8a897a6fb8aa7d7ac795849757
kkkkoki <koki_inoue45@icloud.com>
2023-04-03 20:27:46 +0900

    init

---
+++ src/features/theme/components/ColorSchemaButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState } from 'react';
+import { useMantineColorScheme } from '@mantine/core';
+import { createStyles } from '@mantine/core';
+import { Within } from '@theme-toggles/react';
+import '@theme-toggles/react/css/Within.css';
+
+const useStyles = createStyles((theme) => ({
+  toggleColorSchemaBtn: {
+    display: 'flex',
+    fontSize: 40,
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.orange[5]
+        : theme.colors.indigo[5],
+  },
+}));
+
+const ColorSchemaButton = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+  const [isToggled, setToggle] = useState(!dark);
+  const { classes } = useStyles();
+
+  return (
+    <Within
+      className={classes.toggleColorSchemaBtn}
+      toggled={isToggled}
+      toggle={setToggle}
+      duration={750}
+      onToggle={() => toggleColorScheme()}
+    />
+  );
+};
+
+export default ColorSchemaButton;
